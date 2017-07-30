5dfc473ad92d733a7bb7dcb13bd794cd89e451e5
ben scarborough <benscarborough@scarflix.com>
2017-07-30 14:20:38 -0400

    updated.
    thanks.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,53 @@
-
 class ArticlesController < ApplicationController
+ 
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+    def show
+        @article = Article.find(params[:id])
+    end
 
-  def new
-    @article = Article.new
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def create
-    @article = Article.new(article_params)
-
+    def new
+        @article = Article.new
+    end
+ 
+    def edit
+        @article = Article.find(params[:id])
+    end
+ 
+    
+    def create
+        @article = Article.new(article_params)
+ 
     if @article.save
-      redirect_to @article
+        redirect_to @article
     else
-      render 'new'
+        render 'new'
     end
-  end
-
-  def update
-    @article = Article.find(params[:id])
-
+end
+ 
+    def update
+        @article = Article.find(params[:id])
+ 
     if @article.update(article_params)
-      redirect_to @article
+        redirect_to @article
     else
-      render 'edit'
+        render 'edit'
     end
-  end
+end
 
-  def destroy
+def destroy
     @article = Article.find(params[:id])
     @article.destroy
-
+ 
     redirect_to articles_path
   end
-
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
